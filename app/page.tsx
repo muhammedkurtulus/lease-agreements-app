674fef018defc6de02f772c34f51accc469204d2
muhammedkurtulus <muhammedkurtulus06@gmail.com>
2023-10-25 23:16:59 +0300

    contract integrations read/write

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,115 @@
 "use client";
 
 import { ConnectKitButton } from "connectkit";
-import { useEffect } from "react";
-import { useAccount, useBalance } from "wagmi";
+import { useEffect, useState } from "react";
+import { BaseError } from "viem";
+import { useAccount } from "wagmi";
+import { Lease__factory } from "@/typechain-types";
+import { Address } from "viem";
+import { useContractRead, useContractWrite, useWaitForTransaction } from "wagmi";
+
+const contract = {
+  address: process.env.NEXT_PUBLIC_LEASE_CONTRACT as Address,
+  abi: Lease__factory.abi,
+} as const;
+
+export enum PropertyType {
+  House,
+  Shop,
+}
 
 export default function Home() {
   const { address } = useAccount();
   const { isConnected } = useAccount();
 
+  const {
+    data: properties,
+    isRefetching: loadingProperties,
+    refetch: getAllProperties,
+  } = useContractRead({
+    ...contract,
+    functionName: "getAllProperties",
+  });
+
+  const {
+    write: addProperty,
+    data,
+    error,
+    isLoading,
+    isError,
+  } = useContractWrite({
+    ...contract,
+    functionName: "addProperty",
+  });
+
+  const { isLoading: isPending, isSuccess } = useWaitForTransaction({ hash: data?.hash });
+
   useEffect(() => {
-    console.log(address);
-  }, [address]);
+    console.log("properties", properties);
+  }, [properties]);
+
+  const renderPropertyForm = () => {
+    return (
+      <form
+        className="grid gap-y-5 text-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const propertyAddress = formData.get("propertyAddress") as string;
+          const propertyOwnerName = formData.get("propertyOwnerName") as string;
+          const propertyType = formData.get("propertyType") as unknown as PropertyType;
+          addProperty({ args: [propertyAddress, propertyType, propertyOwnerName] });
+        }}
+      >
+        <input name="propertyAddress" placeholder="Property Address" />
+        <input name="propertyOwnerName" placeholder="Property Owner Name" />
+        <select name="propertyType">
+          <option value={PropertyType.House}>House</option>
+          <option value={PropertyType.Shop}>Shop</option>
+        </select>
+        <button className="bg-white" disabled={isLoading} type="submit">
+          Add
+        </button>
+      </form>
+    );
+  };
+
+  const renderPropertyList = () => {
+    return (
+      <div className="grid w-96 gap-y-5">
+        {properties?.map((property) => {
+          return (
+            <div
+              className="grid justify-start text-white gap-y-1 text-black"
+              key={property.propertyAddress}
+            >
+              <div>Property Address: {property.propertyAddress}</div>
+              <div>Property Owner Name: {property.ownerName}</div>
+              <div>Property Type: {property.propertyType}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const renderTransaction = () => {
+    return (
+      <>
+        {isLoading && <div>Check wallet...</div>}
+        {isPending && <div>Transaction pending...</div>}
+        {isSuccess && <div>Transaction Hash: {data?.hash}</div>}
+        {isError && <div>{(error as BaseError)?.shortMessage}</div>}
+      </>
+    );
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-20">
       <ConnectKitButton />
+      {renderPropertyForm()}
+      {renderPropertyList()}
+      {renderTransaction()}
     </main>
   );
 }
--- utils/wagmi.ts
+++ utils/wagmi.ts
@@ -1,5 +1,6 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig } from "wagmi";
+import { localhost } from "wagmi/chains";
 
 const walletConnectProjectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID!;
 
@@ -8,5 +9,6 @@ export const config = createConfig(
     autoConnect: true,
     appName: "My wagmi + ConnectKit App",
     walletConnectProjectId,
+    chains: [localhost],
   })
 );
